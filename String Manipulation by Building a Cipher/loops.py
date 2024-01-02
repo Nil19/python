6f23b5411f447621bf618baf7e12793df8ded839
Nil19 <nilufar_adat@hotmail.com>
2024-01-02 21:13:16 +0000

    steps 11 - 20

---
+++ String Manipulation by Building a Cipher/loops.py
@@ -0,0 +1,24 @@
+#LOOPS
+
+#step 18: Remove everything after the alphabet line. Then write a for loop to iterate over text.
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for i in text:
+    #step 19: Give your for loop a body by adding a call to print(i). Remember to indent the loop body
+    print(i)
+
+#output:
+#>>> H
+#>>> e
+#>>> l
+#>>> l
+#>>> o
+#>>>  
+#>>> W
+#>>> o
+#>>> r
+#>>> l
+#>>> d   
+    
+#step 20: n/a
--- String Manipulation by Building a Cipher/main.py
+++ String Manipulation by Building a Cipher/main.py
@@ -1,3 +1,5 @@
+#STEPS 1 - 17
+
 #Step 1: Create a variable called number and assign it the value 5.
 number = 5
 
@@ -52,3 +54,41 @@ print(type(shift))
 #output: 
 #class 'str'>
 #<class 'int'>
+
+#step 11: Remove both calls to print() and declare another variable called alphabet. Assign the string abcdefghijklmnopqrstuvwxyz to this variable.
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+#step 12: At the end of your code, call find() on your alphabet string and pass text[0] to the function.
+alphabet.find(text[0])
+
+#step 13: Now assign the value returned by alphabet.find(text[0]) to a variable named index. Then, print its value.
+index = alphabet.find(text[0])
+print(index)
+
+#step 14: Add another print() call to print text.lower() and see the output.
+print (text.lower())
+#output: hello world
+
+#step 15: Remove the last print() call. Then, instead of text[0], pass text[0].lower() to find() and see the output.
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+index = alphabet.find(text[0].lower())
+print(index)
+
+#output: 7
+#note: The output should be 7 because the first character of the string text is 'H', and when converted to lowercase using text[0].lower(), it becomes 'h'. 
+# The lowercase letter 'h' is at index 7 in the string alphabet ('abcdefghijklmnopqrstuvwxyz'). Therefore, the output of print(index) will be 7.
+
+#step 16: Declare a variable named shifted and assign it the alphabet letter at index plus shift.
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+index = alphabet.find(text[0].lower())
+print(index)
+shifted = alphabet[index+shift]
+#step 17: print it
+print(shifted)
+#output: k

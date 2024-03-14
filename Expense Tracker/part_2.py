36cf9e5230e4645d0a1b8236cd7ab161318565b7
Nil19 <nilufar_adat@hotmail.com>
2024-03-14 16:14:22 +0000

    Expense tracker: steps 21-29

--- Expense Tracker/part_2.py
+++ Expense Tracker/part_2.py
@@ -32,3 +32,33 @@ def filter_expenses_by_category(expenses, category):
 #Step 20: Within the filter_expenses_by_category function, replace pass with a lambda function. Use expense as the parameter and return the comparison between the value of the 'category' key of the expense dictionary and category.
 def filter_expenses_by_category(expenses, category):
     lambda expense: expense['category'] == category
+
+#Step 21: Within the filter_expenses_by_category function, call filter() passing the lambda function you wrote in the previous step as the first argument and the expenses list as the second argument.    
+def filter_expenses_by_category(expenses, category):
+    filter(lambda expense: expense['category'] == category, expenses)
+
+#Step 22: Finally, return the result of the filter() call
+    return filter(lambda expense: expense['category'] == category, expenses)
+
+#Step 23: Define a function named main without parameters. Fill the function body with the expenses list you created at the beginning of this project. You will use this list to store the expense records.
+def main():
+    expenses
+    pass
+expenses = []
+
+#Step 24: create a while loop. Use True for the condition, and print the string '\nExpense Tracker' inside the loop body to show the title of the program.
+#Step 25: After the print() call, add another one to print the string '1. Add an expense'.
+#Step 26: Next, add another print() call and pass the string '2. List all expenses'.
+#Step 27: Provide the other menu options by printing the following three strings in your while loop: '3. Show total expenses', '4. Filter expenses by category', and '5. Exit'.
+def main():
+    expenses = []
+    while True: 
+        print("\nExpense Tracker")
+        print('1. Add an expense')
+        print('2. List all expenses')
+        print('3. Show total expenses')
+        print('4. Filter expenses by category')
+        print('5. Exit')
+
+#Step 28: Inside your while loop, call the input() function passing the string 'Enter your choice: ' as the argument, and assign the result to a variable named choice.
+        choice = input('Enter your choice: ')

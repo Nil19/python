33422e023b93cfc9aa37ecd92e0ea155bb2d2d5a
Nilufar Adat <51162107+Nil19@users.noreply.github.com>
2024-06-24 16:30:56 +0000

    Case converter

---
+++ Case Converter/part_1.py
@@ -0,0 +1,13 @@
+#Step 1: define a new function named convert_to_snake_case() that takes pascal_or_camel_cased_string as input. 
+#Within the function body, include a pass statement to temporarily fill the function body.
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    pass
+
+#Step 2: Inside the function, replace the pass statement by creating an empty list named snake_cased_char_list.
+    snake_cased_char_list = []
+
+#Step 3: Inside the function, below the list you just created, add a for loop to iterate through the pascal_or_camel_cased_string. 
+#Make sure to name the target variable char. For now, add a pass statement as a placeholder in the loop body.
+    for char in pascal_or_camel_cased_string:
+
+        pass

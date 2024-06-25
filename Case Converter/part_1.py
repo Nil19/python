5e2e86ecbb3d308df06524acfc52adb1945ef208
Nilufar Adat <51162107+Nil19@users.noreply.github.com>
2024-06-25 15:08:20 +0000

    part 3 - 10 completion

--- Case Converter/part_1.py
+++ Case Converter/part_1.py
@@ -11,3 +11,32 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     for char in pascal_or_camel_cased_string:
 
         pass
+
+#Step 4: Inside the for loop, add an if statement to check if the current character is uppercase. Move the pass statement inside the new if statement.
+        if char.isupper():
+        
+            pass
+
+#Step 5: Use the .lower() string method to convert uppercase characters to lowercase characters. 
+#Then, prepend an underscore to the character. Assign the results to a variable named converted_character
+        if char.isupper():
+           '_' + char.lower()
+           converted_character = '_' + char.lower()
+
+#Step 6: Use the .append() method on the snake_cased_char_list to add the converted_character to the list.
+            snake_cased_char_list.append(converted_character)
+
+#Step 7: Right after the if statement within the for loop, add an else clause and use the .append() method to add char to the snake_cased_char_list variable.
+        else:
+            snake_cased_char_list.append(char)
+
+#Step 8: use the .join() method to join the elements in snake_cased_char_list using an empty string as the separator. 
+#Assign the result to a new variable named snake_cased_string.
+    snake_cased_string = ''.join(snake_cased_char_list)
+
+#Step 9: eclare a new variable named clean_snake_cased_string and assign it the result of the .strip() method applied to snake_cased_string , 
+#passing '_' as the argument to the method.
+    clean_snake_cased_string = snake_cased_string.strip('_')
+
+#Step 10: Add a return statement at the end of the function to return the clean_snake_cased_string.
+    return clean_snake_cased_string
